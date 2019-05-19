d243cb19585999fa4c5ec04c28e155cb4b31f415
Robert Andrade <randrade@Roberts-MacBook-Pro-RH.local>
2019-05-19 10:32:34 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>YEET</h1>
+        <img src="https://scontent-lga3-1.cdninstagram.com/vp/4c559794eb62cafcebe7f883b173a04c/5D65B56D/t51.2885-15/sh0.08/e35/s640x640/41203721_691517357888913_5430100155676178400_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com" className="App-logo" alt="logo" />
+        <p>
+          Let's have a great wedding
+        </p>
+        
+        <a
+          className="App-link"
+          href="https://www.instagram.com/me_hoy_minoyyy/?hl=en"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          ILYSM
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
