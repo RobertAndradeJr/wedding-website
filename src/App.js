3e28748472b33fc65b2a4bc8030d85f4230df196
Robert Andrade <randrade@Roberts-MacBook-Pro-RH.local>
2019-05-19 10:36:22 -0500

    changed title

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>YEET</h1>
+        <h1>Wedding Time</h1>
         <img src="https://scontent-lga3-1.cdninstagram.com/vp/4c559794eb62cafcebe7f883b173a04c/5D65B56D/t51.2885-15/sh0.08/e35/s640x640/41203721_691517357888913_5430100155676178400_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com" className="App-logo" alt="logo" />
         <p>
           Let's have a great wedding
