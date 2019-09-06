ee0d396b4161fdab7fb9d256ab1535d79a09d421
Robert Andrade <srandradejr@gmail.com>
2019-09-06 15:58:11 -0500

    HeartLogo Component

--- src/components/Nav/NavHeader.js
+++ src/components/Nav/NavHeader.js
@@ -1,18 +1,11 @@
 import React from 'react'
 import titleText from '../Strings/NavStrings'
+import HeartLogo from './HeartLogo'
 
 export default function NavHeader() {
   return (
     <div className="flex items-center flex-shrink-0 text-white mr-6">
-      <svg
-        className="fill-current h-8 w-8 mr-2"
-        width="54"
-        height="54"
-        viewBox="0 0 54 54"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M12.76 3.76a6 6 0 0 1 8.48 8.48l-8.53 8.54a1 1 0 0 1-1.42 0l-8.53-8.54a6 6 0 0 1 8.48-8.48l.76.75.76-.75zm7.07 7.07a4 4 0 1 0-5.66-5.66l-1.46 1.47a1 1 0 0 1-1.42 0L9.83 5.17a4 4 0 1 0-5.66 5.66L12 18.66l7.83-7.83z" />
-      </svg>
+      <HeartLogo />
       <span className="font-semibold text-xl tracking-tight">{titleText}</span>
     </div>
   )
