a6de5668a4f72fb1ea5abf7857cb8c9c15cd2d79
Robert Andrade <srandradejr@gmail.com>
2019-09-16 14:13:36 -0500

    change NavStrings

--- src/components/Strings/NavStrings.js
+++ src/components/Strings/NavStrings.js
@@ -1,6 +1,5 @@
 export const mobileTitle = 'Menu'
 export const titleText = 'Robert + Mandi'
-export const leftButtons = ['Events', 'Instagram', 'Engagement', 'Glimpse of the city']
+export const leftButtons = ['How We Met', 'Events', 'Instagram', 'Engagement', 'Glimpse of the city']
 export const rightButtons = ['venue', 'RSVP']
 export const test = 'yeet'
-export default test
