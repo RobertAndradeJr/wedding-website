2b2666862b4c2de14eb6cd52c06bf023f6cdd1bd
Robert Andrade <srandradejr@gmail.com>
2019-10-17 17:51:22 -0500

    navbar over everything

--- src/components/Nav/Navbar.js
+++ src/components/Nav/Navbar.js
@@ -5,7 +5,7 @@ import NavHeader from './NavHeader'
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-yellow-500 p-6 fixed w-full">
+    <nav className="flex items-center justify-between flex-wrap bg-yellow-500 p-6 fixed w-full z-50">
       <NavHeader />
       <MobileNavButtons />
       <NavButtons />
