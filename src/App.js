f032c105c6da37067c92289da9d9d8fbca9eae4d
Robert Andrade <srandradejr@gmail.com>
2019-10-17 15:27:58 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,50 +1,51 @@
-import React, { useState } from 'react'
-import Home from './components/Home'
+import React from 'react'
+// import Home from './components/Home'
 
-const formTitle = 'Password:'
+// const formTitle = 'Password:'
 
-function Form(props) {
-  const { onChange, handleSubmit } = props
-  return (
-    <form>
-      {formTitle}
-      <input type="text" onChange={onChange} />
-      <input type="submit" onClick={handleSubmit} />
-    </form>
-  )
-}
+// function Form(props) {
+//   const { onChange, handleSubmit } = props
+//   return (
+//     <form>
+//       {formTitle}
+//       <input type="text" onChange={onChange} />
+//       <input type="submit" onClick={handleSubmit} />
+//     </form>
+//   )
+// }
 
 function App() {
   const correctPassword = 'wedding'
-  const [input, setInput] = useState('')
-  const [message, setMessage] = useState('')
-  const [password, setPassword] = useState(false)
-  const checkLocalHost = () => {
-    if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
-      return true // eslint-disable-line no-console
-    }
-    return false
-  }
+  // const [input, setInput] = useState('')
+  // const [message, setMessage] = useState('')
+  // const [password, setPassword] = useState(false)
+  // const checkLocalHost = () => {
+  //   if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
+  //     return true
+  //   }
+  //   return false
+  // }
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    if (input === correctPassword) {
-      setPassword(true)
-    } else {
-      setMessage('Sorry! Only COOL people can come to our wedding')
-      setInput('')
-    }
-  }
-  const onChange = e => setInput(e.target.value)
+  // const handleSubmit = (e) => {
+  //   e.preventDefault()
+  //   if (input === correctPassword) {
+  //     setPassword(true)
+  //   } else {
+  //     setMessage('Sorry! Only COOL people can come to our wedding')
+  //     setInput('')
+  //   }
+  // }
+  // const onChange = (e) => setInput(e.target.value)
 
   return (
     <div className="App">
-      { (checkLocalHost() || password) ? <Home /> : (
+      {/* { (checkLocalHost() || password) ? <Home /> : (
         <div>
           <h1>{message}</h1>
           <Form handleSubmit={handleSubmit} onChange={onChange} />
         </div>
-      )}
+      )} */}
+      <h1>{correctPassword}</h1>
     </div>
   )
 }
