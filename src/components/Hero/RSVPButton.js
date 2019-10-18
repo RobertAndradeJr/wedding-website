0ac39d754759aedd2c035546d53931fdcfa7de0e
Robert Andrade <srandradejr@gmail.com>
2019-10-18 15:46:03 -0500

    Buttons extracted to components

--- src/components/Hero/RSVPButton.js
+++ src/components/Hero/RSVPButton.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { HomeStrings } from '../Strings/Strings'
 
-const styles = 'text-white text-2xl rounded-full  py-4 px-8 bg-purple-500 bg-purple-100 hover:bg-purple-400 focus:outline-none focus:shadow-outline active:bg-purple-800 '
+const styles = 'btn btn--large btn--purple hover:bg-purple-400 focus:outline-none focus:shadow-outline active:bg-purple-800'
 const { buttonText } = HomeStrings
 
 const RSVPButton = () => (
