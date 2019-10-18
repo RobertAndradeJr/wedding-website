38087827e9fd59f628657e1cd2ae4e340d6f591a
Robert Andrade <srandradejr@gmail.com>
2019-10-18 17:32:40 -0500

    linting error

--- src/components/Nav/Navbar.js
+++ src/components/Nav/Navbar.js
@@ -8,7 +8,9 @@ export default function Navbar() {
   const [NavOpen, setNavOpen] = useState(false)
   const { width } = useWindowDimensions()
   const clickHandler = () => setNavOpen(!NavOpen)
-  const openNav = () => (NavOpen === true || width > 1024 ? <NavButtons clickHandler={clickHandler} /> : null)
+  const openNav = () => (NavOpen === true || width > 1024
+    ? <NavButtons clickHandler={clickHandler} />
+    : null)
 
   return (
     <nav className="flex items-center justify-between flex-wrap bg-yellow-500 p-6 fixed w-full z-50">
