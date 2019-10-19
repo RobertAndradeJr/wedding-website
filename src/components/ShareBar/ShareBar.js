890da1e85569a30ea4891ecd700b367c89f59d4c
Robert Andrade <srandradejr@gmail.com>
2019-10-19 01:42:51 -0500

    Twitter test

--- src/components/ShareBar/ShareBar.js
+++ src/components/ShareBar/ShareBar.js
@@ -5,7 +5,8 @@ const { TWITTER } = ShareBarStrings
 const ShareBar = () => (
   <div className="flex items-center justify-around py-6 ">
     <div className="fb-like" data-href="https://robertandmandi.wedding/" data-width="50" data-layout="button" data-action="like" data-size="small" data-show-faces="true" data-share="true" />
-    <a href="https://twitter.com/intent/tweet?button_hashtag=RobertMandiWedding&ref_src=twsrc%5Etfw" className="twitter-hashtag-button" data-url="https://robertandmandi.wedding" data-show-count="false">{TWITTER}</a>
+    <a href="https://twitter.com/intent/tweet?button_hashtag=RobertandMandiWedding&ref_src=twsrc%5Etfw" className="twitter-hashtag-button" data-show-count="false">{TWITTER}</a>
+    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8" />
   </div>
 )
 
