1f8d61a23d64c5d10ee04d12ff8ae4a92bcddb1f
Robert Andrade <srandradejr@gmail.com>
2019-10-20 17:29:07 -0500

    invitation placeholder

--- src/components/PageSections/InvitationSection.js
+++ src/components/PageSections/InvitationSection.js
@@ -2,10 +2,11 @@ import React from 'react'
 import ScrollableAnchor from 'react-scrollable-anchor'
 import ShareBar from '../ShareBar/ShareBar'
 
+const PLACEHOLDER = 'Placeholder for invitation'
 const InvitationSection = () => (
   <ScrollableAnchor id="invitation">
-    <section className="flex flex-col text-white items-center">
-      <ShareBar />
+    <section className="flex flex-col h-screen text-white items-center bg-blue-300">
+      <h1>{PLACEHOLDER}</h1>
     </section>
   </ScrollableAnchor>
 
