475a89ada6f3b26e706cca86a360208bf3a025ef
Robert Andrade <srandradejr@gmail.com>
2019-10-20 17:28:07 -0500

    change hashtags in sharebar

--- src/components/ShareBar/ShareBar.js
+++ src/components/ShareBar/ShareBar.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import { ShareBarStrings } from '../Strings/Strings'
+import { ShareBarStrings, InstagramPicsStrings } from '../Strings/Strings'
 
 const { TWITTER } = ShareBarStrings
+const { HASHTAG } = InstagramPicsStrings
+
+const TWITTER_LINK = `https://twitter.com/intent/tweet?button_hashtag=${HASHTAG}&ref_src=twsrc%5Etfw`
+const WEBSITE_LINK = 'https://robertandmandi.wedding'
 const ShareBar = () => (
   <div className="flex items-center justify-around py-6 ">
-    <div className="fb-like" data-href="https://robertandmandi.wedding" data-width="50" data-layout="button" data-action="like" data-size="small" data-show-faces="true" data-share="true" />
-    <a href="https://twitter.com/intent/tweet?button_hashtag=RobertandMandiWedding&ref_src=twsrc%5Etfw" className="twitter-hashtag-button" data-show-count="false">{TWITTER}</a>
+    <div className="fb-like" data-href={WEBSITE_LINK} data-width="50" data-layout="button" data-action="like" data-size="small" data-show-faces="true" data-share="true" />
+    <a href={TWITTER_LINK} className="twitter-hashtag-button" data-show-count="false">{TWITTER}</a>
     <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8" />
   </div>
 )
