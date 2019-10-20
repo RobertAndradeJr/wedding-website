2de8058651b1521928e5395f100536ee7f0930ec
Robert Andrade <srandradejr@gmail.com>
2019-10-20 17:28:40 -0500

    move sharebar to social section

--- src/components/PageSections/InstagramPicsSection.js
+++ src/components/PageSections/InstagramPicsSection.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Axios from 'axios'
 import ScrollableAnchor from 'react-scrollable-anchor'
 import { InstagramPicsStrings } from '../Strings/Strings'
+import ShareBar from '../ShareBar/ShareBar'
 
 const { HASHTAG, HELP_US, PICTURE_WORTH_THOUSAND_WORDS } = InstagramPicsStrings
 
@@ -19,24 +20,30 @@ const InstagramPicsSection = () => {
   }, [])
 
   return (
-    <ScrollableAnchor id="Instagram">
+    <ScrollableAnchor id="Social">
       <section className="bg-yellow-100 justify-center md:px-24 lg:px-48">
         <div className="text-center">
           <h3>{PICTURE_WORTH_THOUSAND_WORDS}</h3>
           <p>
             {HELP_US}
-            <a
-              href={INSTAGRAM_LINK}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <a href={INSTAGRAM_LINK} target="_blank" rel="noopener noreferrer">
               {HASHTAG}
             </a>
           </p>
         </div>
+        <section className="flex flex-col text-white justify-around">
+          <ShareBar />
+        </section>
         <div className="flex flex-row flex-wrap justify-between">
           <>
-            {Pictures.map((pic) => <img key={pic.node.id} src={pic.node.display_url} alt="yeet" className="w-4/12 p-6" />)}
+            {Pictures.map((pic) => (
+              <img
+                key={pic.node.id}
+                src={pic.node.display_url}
+                alt="yeet"
+                className="w-4/12 p-6"
+              />
+            ))}
           </>
         </div>
       </section>
