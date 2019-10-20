c9d68728f86c09a92f135db826b6ab024d65e63b
Robert Andrade <srandradejr@gmail.com>
2019-10-20 17:29:02 -0500

    added video to city section

--- src/components/PageSections/CitySection.js
+++ src/components/PageSections/CitySection.js
@@ -1,21 +1,24 @@
 import React from 'react'
 import ScrollableAnchor from 'react-scrollable-anchor'
+import YouTubeBackground from 'react-youtube-background'
 import { CityStrings } from '../Strings/Strings'
 
 const { CITY_NAME, CITY_MOTTO } = CityStrings
 
+
 const CitySection = () => (
   <ScrollableAnchor id="Glimpseofthecity">
-    <section className="h-screen bg-green-100">
-      <div
-        id="bgndVideo"
-        className="player"
-        data-property="{videoURL:'https://youtu.be/CWzZ-Y2D9tw',containment:'#video-bg',autoPlay:true, mute:true, showControls:false, startAt:77, stopAt:259, opacity:1}"
-      />
-      <div id="video-content">
-        <h5>{CITY_NAME}</h5>
-        <p>{CITY_MOTTO}</p>
-      </div>
+    <section className="relative my-24">
+      <YouTubeBackground
+        videoId="CWzZ-Y2D9tw"
+        className="z-30 h-64 flex flex-col justify-center text-white"
+        overlay="rgba(0,0,0,0.5)"
+      >
+        <div id="video-content">
+          <h5 className="text-4xl">{CITY_NAME}</h5>
+          <p className="text-xl">{CITY_MOTTO}</p>
+        </div>
+      </YouTubeBackground>
     </section>
   </ScrollableAnchor>
 )
