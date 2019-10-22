d5e4622c005c9d976577975a8d64fffcd5256aef
Robert Andrade <srandradejr@gmail.com>
2019-10-22 13:58:52 -0500

    bell museum background image

--- src/components/PageSections/DirectionsSection.js
+++ src/components/PageSections/DirectionsSection.js
@@ -16,13 +16,20 @@ const {
 
 const BELL_MUSEUM_TEL_LINK = `tel:${CONTACT_NUMBER}`
 const UBER_LINK = 'https://m.uber.com/ul/?action=setPickup&client_id=WQvddpr5mKEbJ_eFl1uvVOQvi_LCEPWe&pickup=my_location&dropoff[formatted_address]=Bell%20Museum%2C%20Larpenteur%20Avenue%20West%2C%20Saint%20Paul%2C%20MN%2C%20USA&dropoff[latitude]=44.991158&dropoff[longitude]=-93.187898'
-
 const GOOGLE_MAPS_LINK = 'https://g.page/BellMuseum?share'
 
 const DirectionsSection = () => (
   <ScrollableAnchor id="Directions">
-    <section className="flex flex-row justify-around pb-24">
-      <div className="w-9/12 md:w-6/12 flex flex-row bg-gray-400 justify-around p-8">
+    <section
+      className="flex flex-row justify-around py-24 text-white"
+      style={{
+        backgroundImage: `linear-gradient(
+        rgba(0, 0, 0, 0.5), 
+        rgba(0, 0, 0, 0.5)
+      ),url(https://www.bellmuseum.umn.edu/wp-content/uploads/2019/02/horizon-hall-hero.jpg)`,
+      }}
+    >
+      <div className="w-9/12 md:w-6/12 flex flex-row justify-around p-8">
         <div className="flex flex-col">
           <h3 className="text-xl">{VENUE_NAME}</h3>
           <p>{VENUE_ADDRESS}</p>
@@ -38,7 +45,14 @@ const DirectionsSection = () => (
           <p>{CONTACT_NAME}</p>
           <a className="text-yellow-600" href={BELL_MUSEUM_TEL_LINK}>{CONTACT_NUMBER}</a>
           <a className="pt-8" href={GOOGLE_MAPS_LINK}>
-            <button type="button" className="btn bg-yellow-500 hover:bg-yellow-400 active:bg-yellow-600">
+            <button type="button" className="btn bg-yellow-500 hover:bg-yellow-400 active:bg-yellow-600 flex justify-around">
+              <svg className="inline my-1 mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18">
+                <path
+                  style={{ fill: '#fff' }}
+                  className="heroicon-ui"
+                  d="M5.64 16.36a9 9 0 1 1 12.72 0l-5.65 5.66a1 1 0 0 1-1.42 0l-5.65-5.66zm11.31-1.41a7 7 0 1 0-9.9 0L12 19.9l4.95-4.95zM12 14a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm0-2a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"
+                />
+              </svg>
               {SHOW_MAP}
             </button>
           </a>
--- src/components/PageSections/MapSection.js
+++ src/components/PageSections/MapSection.js
@@ -6,12 +6,9 @@ import { MapStrings } from '../Strings/Strings'
 const {
   HOW_DO_I_GET_THERE,
   EASIER_THAN_YOU_THINK,
-  SHOW_INFO,
-  VENUE_ADDRESS,
-  VENUE_NAME,
-  BOOK_UBER,
-  SHOW_MAP,
 } = MapStrings
+
+const MAP_URL = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2821.6627383459654!2d-93.19008618454988!3d44.991161972903036!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x52b32d172d3e3a05%3A0x63d7830833a56d3b!2sBell%20Museum!5e0!3m2!1sen!2sus!4v1571612382679!5m2!1sen!2sus'
 
 const DirectionsSection = () => (
   <ScrollableAnchor id="Venue">
@@ -21,7 +18,15 @@ const DirectionsSection = () => (
         <p className="text-xl">{EASIER_THAN_YOU_THINK}</p>
       </div>
       <div className="h-48 flex flex-row justify-around" id="map-container">
-        <iframe className="w-9/12" title="bell-museum-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2821.6627383459654!2d-93.19008618454988!3d44.991161972903036!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x52b32d172d3e3a05%3A0x63d7830833a56d3b!2sBell%20Museum!5e0!3m2!1sen!2sus!4v1571612382679!5m2!1sen!2sus" height="450" frameBorder="0" style={{ border: 0 }} allowFullScreen="" />
+        <iframe
+          className="w-9/12"
+          title="bell-museum-map"
+          src={MAP_URL}
+          height="450"
+          frameBorder="0"
+          style={{ border: 0 }}
+          allowFullScreen=""
+        />
       </div>
     </section>
   </ScrollableAnchor>
