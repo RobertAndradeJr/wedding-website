8957e07f992cf6a9f1397eb1c50a6c4a08410643
Robert Andrade <srandradejr@gmail.com>
2019-10-23 03:07:39 -0500

    invitation section placeholder illustration

--- src/components/PageSections/InvitationSection.js
+++ src/components/PageSections/InvitationSection.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ScrollableAnchor from 'react-scrollable-anchor'
+import InvitationIllustration from '../../assets/undraw_invite.svg'
 
-const PLACEHOLDER = 'Placeholder for invitation'
+const PLACEHOLDER = 'Please join us for a night of fun festivities'
 const InvitationSection = () => (
   <ScrollableAnchor id="invitation">
-    <section className="flex flex-col h-screen text-white items-center bg-blue-300">
-      <h1>{PLACEHOLDER}</h1>
+    <section className="flex flex-col text-white items-center p-24">
+      <h1 className="text-4xl py-4">{PLACEHOLDER}</h1>
+      <img src={InvitationIllustration} className="w-6/12" alt="invitation illustration" />
     </section>
   </ScrollableAnchor>
 
