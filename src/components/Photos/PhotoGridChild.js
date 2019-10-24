2dd524d48f6837ac0e87dcf18ce021ca51258bc6
Robert Andrade <srandradejr@gmail.com>
2019-10-24 15:44:24 -0500

    photogridchild component

---
+++ src/components/Photos/PhotoGridChild.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { useArrowNavigationWithFocusState } from 'react-arrow-navigation'
+
+const PhotoGridChild = ({
+  index,
+  item,
+  handleClick,
+  styles,
+}) => {
+  const {
+    focusProps: { ref, tabIndex, onClick },
+  } = useArrowNavigationWithFocusState(0, index)
+  return (
+    <button
+      type="button"
+      ref={ref}
+      tabIndex={tabIndex}
+      className={styles.join(' ')}
+      onClick={() => {
+        onClick()
+        handleClick(item)
+      }}
+      style={{ transition: 'padding 700ms' }}
+    >
+      <img src={item} alt="engagement" />
+    </button>
+  )
+}
+
+PhotoGridChild.defaultProps = {
+  index: PropTypes.number,
+  item: PropTypes.string,
+  handleClick: PropTypes.func,
+  styles: PropTypes.array,
+}
+
+PhotoGridChild.propTypes = {
+  index: PropTypes.number,
+  item: PropTypes.string,
+  handleClick: PropTypes.func,
+  styles: PropTypes.array,
+}
+
+export default PhotoGridChild
