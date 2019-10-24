331996b7851febabf1749aa7d88baaa771dff6ec
Robert Andrade <srandradejr@gmail.com>
2019-10-23 22:51:29 -0500

    change engagement pic from modal zoom view

--- src/components/PageSections/EngagementPicsSection.js
+++ src/components/PageSections/EngagementPicsSection.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import Modal from 'react-modal'
 import ScrollableAnchor from 'react-scrollable-anchor'
 import { EngagementPicsStrings } from '../Strings/Strings'
 import TravelingIllustration from '../../assets/undraw_travelers.svg'
@@ -8,61 +7,28 @@ const { SUBTITLE, TITLE } = EngagementPicsStrings
 const importAll = (r) => r.keys().map(r)
 const images = importAll(require.context('../../assets/engagement-photos', false, /\.(png|jpe?g|svg)$/))
 
-
-const ModalStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    width: '50%',
-  },
-  overlay: {
-    backgroundColor: 'rgba(0, 0, 0, 0.75)',
-
-  },
-}
-
-
 const EngagementPicsSection = () => {
   const [selectedImage, setSelectedImage] = useState(null)
-  const [OpenModal, setOpenModal] = useState(false)
-  const handleClick = (image) => {
-    setSelectedImage(image)
-    setOpenModal(true)
-  }
-
-
+  // const handleClick = (image) => setSelectedImage(image)
+  const handleFocus = (image) => setSelectedImage(image)
+  const handleBlur = () => setSelectedImage(null)
   return (
     <ScrollableAnchor id="Engagement">
       <section className="h-auto flex flex-col items-center pb-8">
         <h2 className="text-4xl text-white py-8">{TITLE}</h2>
         <h2 className="text-lg text-white py-8">{SUBTITLE}</h2>
         <div className=" flex flex-row items-center justify-between w-screen">
-          <div className="float-left w-4/12 flex flex-col justify-around">
-            <img src={TravelingIllustration} alt="two travelers" />
+          <div className="float-left w-4/12 flex flex-col justify-around max-h-full">
+            <img src={selectedImage === null ? TravelingIllustration : selectedImage} alt="two travelers" className="max-h-full" />
           </div>
           <div className="float-right w-6/12 flex flex-row flex-wrap clearfix">
             {
               images.map((image) => (
-                <button key={image} type="button" className="w-2/12 p-2 hover:p-0" style={{ transition: 'padding 700ms' }} onClick={() => handleClick(image)}>
+                <button key={image} type="button" onFocus={() => handleFocus(image)} onBlur={() => handleBlur()} className="w-2/12 p-2 hover:p-0" style={{ transition: 'padding 700ms' }}>
                   <img src={image} alt="engagement" />
                 </button>
               ))
             }
-            <Modal
-              isOpen={OpenModal === true}
-              // onAfterOpen={this.afterOpenModal}
-              onRequestClose={() => setOpenModal(false)}
-              style={ModalStyles}
-              contentLabel={selectedImage}
-            >
-              <div className="text-center">
-                <img src={selectedImage} alt="engagement detail view" className="w-full" />
-              </div>
-            </Modal>
           </div>
         </div>
       </section>
