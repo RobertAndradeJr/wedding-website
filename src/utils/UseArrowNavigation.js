46c3e0083e8bacd5a319ebf78beb6f7fd44d35cd
Robert Andrade <srandradejr@gmail.com>
2019-10-23 22:53:09 -0500

    Don't need ot import react on useArrowNavigation hook

--- src/utils/UseArrowNavigation.js
+++ src/utils/UseArrowNavigation.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const UseArrowNavigation = (targetKey) => {
   const [keyPressed, setKeyPressed] = useState(false)
