f87c1698f4343dbaee82562940b506cfad0a2b23
Robert Andrade <srandradejr@gmail.com>
2019-10-23 22:50:45 -0500

    hover change whether navbar is clear or not

--- src/components/Nav/NavHeader.js
+++ src/components/Nav/NavHeader.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import { NavStrings } from '../Strings/Strings'
 import HeartLogo from './HeartLogo'
+import useWindowDimensions from '../../utils/UseWindowDimensions'
+import UseScrollSpy from '../../utils/UseScrollSpy'
 
 const { titleText } = NavStrings
+const styles = [
+  'flex',
+  'items-center',
+  'flex-shrink-0',
+  'text-white',
+  'mr-6',
+  'focus:outline-none',
+  'focus:shadow-outline',
+]
 
 export default function NavHeader() {
+  const width = useWindowDimensions()
+  const scroll = UseScrollSpy()
   return (
     <a href="#Home" className="focus:outline-none">
-      <button className="flex items-center flex-shrink-0 text-white mr-6 focus:outline-none focus:shadow-outline" type="button">
+      <button className={styles.join(' ') + (width < 1024 || scroll === true ? ' hover:text-gray-600' : ' hover:text-yellow-500')} type="button">
         <HeartLogo />
-        <span className="font-semibold text-xl tracking-tight hover:text-black">{titleText}</span>
+        <span className="font-semibold text-xl tracking-tight">{titleText}</span>
       </button>
     </a>
   )
