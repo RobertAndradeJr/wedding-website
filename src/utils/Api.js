61387004c8d5dca69d917f7a752876409a9e8e83
Robert Andrade <srandradejr@gmail.com>
2019-10-25 20:05:38 -0500

    added fauna functions frontend

---
+++ src/utils/Api.js
@@ -0,0 +1,30 @@
+const create = (data) => fetch('/.netlify/functions/rsvps-create', {
+  body: JSON.stringify(data),
+  method: 'POST',
+}).then((response) => response.json())
+
+const readAll = () => fetch('/.netlify/functions/rsvps-read-all').then((response) => response.json())
+
+const update = (rsvpId, data) => fetch(`/.netlify/functions/rsvps-update/${rsvpId}`, {
+  body: JSON.stringify(data),
+  method: 'POST',
+}).then((response) => response.json())
+
+const deleteRsvp = (rsvpId) => fetch(`/.netlify/functions/rsvps-delete/${rsvpId}`, {
+  method: 'POST',
+}).then((response) => response.json())
+
+const batchDeleteRsvp = (rsvpIds) => fetch('/.netlify/functions/rsvps-delete-batch', {
+  body: JSON.stringify({
+    ids: rsvpIds,
+  }),
+  method: 'POST',
+}).then((response) => response.json())
+
+export default {
+  create,
+  readAll,
+  update,
+  delete: deleteRsvp,
+  batchDelete: batchDeleteRsvp,
+}
