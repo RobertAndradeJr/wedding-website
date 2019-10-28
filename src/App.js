c31813b12165fc80d969f716b5d8aed457aa0487
Robert Andrade <srandradejr@gmail.com>
2019-10-28 06:14:35 -0500

    Tuened off password for now

--- src/App.js
+++ src/App.js
@@ -1,44 +1,45 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Home from './components/Home'
 
-const formTitle = 'Password:'
+// const formTitle = 'Password:'
 
 function App() {
-  const correctPassword = 'wedding'
-  const [input, setInput] = useState('')
-  const [message, setMessage] = useState('')
-  const [password, setPassword] = useState(false)
-  const checkLocalHost = () => {
-    if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
-      return true
-    }
-    return false
-  }
+  // const correctPassword = 'wedding'
+  // const [input, setInput] = useState('')
+  // const [message, setMessage] = useState('')
+  // const [password, setPassword] = useState(false)
+  // const checkLocalHost = () => {
+  //   if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
+  //     return true
+  //   }
+  //   return false
+  // }
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    if (input === correctPassword) {
-      setPassword(true)
-    } else {
-      setMessage('Sorry! Only COOL people can come to our wedding')
-      setInput('')
-    }
-  }
-  const onChange = (e) => setInput(e.target.value)
+  // const handleSubmit = (e) => {
+  //   e.preventDefault()
+  //   if (input === correctPassword) {
+  //     setPassword(true)
+  //   } else {
+  //     setMessage('Sorry! Only COOL people can come to our wedding')
+  //     setInput('')
+  //   }
+  // }
+  // const onChange = (e) => setInput(e.target.value)
 
   return (
-    <div className="h-screen">
-      { (checkLocalHost() || password) ? <Home /> : (
-        <div>
-          <h1>{message}</h1>
-          <form>
-            {formTitle}
-            <input className="text-black" type="text" onChange={onChange} />
-            <input className="text-black" type="submit" onClick={handleSubmit} />
-          </form>
-        </div>
-      )}
-    </div>
+    // <div className="h-screen">
+    //   { (checkLocalHost() || password) ? <Home /> : (
+    //     <div>
+    //       <h1>{message}</h1>
+    //       <form>
+    //         {formTitle}
+    //         <input className="text-black" type="text" onChange={onChange} />
+    //         <input className="text-black" type="submit" onClick={handleSubmit} />
+    //       </form>
+    //     </div>
+    //   )}
+    // </div>
+    <Home />
   )
 }
 
