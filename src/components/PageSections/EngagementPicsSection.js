05958f13174446a757b2791e920ea6d8f83c7726
Robert Andrade <srandradejr@gmail.com>
2019-10-31 16:00:41 -0500

    move picture loading to custom hook

--- src/components/PageSections/EngagementPicsSection.js
+++ src/components/PageSections/EngagementPicsSection.js
@@ -3,10 +3,9 @@ import ScrollableAnchor from 'react-scrollable-anchor'
 import { EngagementPicsStrings } from '../Strings/Strings'
 import TravelingIllustration from '../../assets/undraw_travelers.svg'
 import PhotoGrid from '../Photos/PhotoGrid'
+import UseImageLoader from '../../utils/UseImageLoader'
 
 const { SUBTITLE, TITLE } = EngagementPicsStrings
-const importAll = (r) => r.keys().map(r)
-const items = importAll(require.context('../../assets/engagement-photos', false, /\.(png|jpe?g|svg)$/))
 
 const gridStyles = [
   'w-2/12',
@@ -17,6 +16,7 @@ const gridStyles = [
 const EngagementPicsSection = () => {
   const [selectedImage, setSelectedImage] = useState(null)
   const handleClick = (image) => setSelectedImage(image)
+  const items = UseImageLoader()
 
   return (
     <ScrollableAnchor id="Engagement">
@@ -27,7 +27,7 @@ const EngagementPicsSection = () => {
           <div className="float-left w-4/12 flex flex-col justify-around max-h-full">
             <img src={selectedImage === null ? TravelingIllustration : selectedImage} alt="two travelers" className="max-h-full" />
           </div>
-          <PhotoGrid items={items} handleClick={handleClick} gridStyles={gridStyles} />
+          {items && <PhotoGrid items={items} handleClick={handleClick} gridStyles={gridStyles} />}
         </div>
       </section>
     </ScrollableAnchor>
