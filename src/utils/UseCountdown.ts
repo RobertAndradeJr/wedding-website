3760330de03b53d5a10453ce9f716f8cdab7125b
Robert Andrade <sandrade@wiley.com>
2019-12-28 03:54:05 -0600

    countdown

---
+++ src/utils/UseCountdown.ts
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react'
+
+const getDays = (timeRemaining: number) => Math.floor(
+  timeRemaining / (1000 * 60 * 60 * 24),
+)
+const getHours = (timeRemaining: number) => Math.floor(
+  (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+)
+const getMinutes = (timeRemaining: number) => Math.floor(
+  (timeRemaining % (1000 * 60 * 60)) / (1000 * 60),
+)
+const getSeconds = (timeRemaining: number) => Math.floor(
+  (timeRemaining % (1000 * 60)) / (1000),
+)
+
+const UseCountdown = (date: any, options: any = {}) => {
+  const { intervalTime = 1000, now = () => Date.now() } = options
+  const [timeRemaining, setTimeRemaining] = useState(() => Number(new Date(date)) - Number(new Date(now())))
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeRemaining((current) => {
+        if (current <= 0) {
+          clearInterval(interval)
+
+          return 0
+        }
+
+        return current - intervalTime
+      })
+    }, intervalTime)
+
+    return () => clearInterval(interval)
+  }, [intervalTime])
+
+  const humanReadableTime = {
+    Days: getDays(timeRemaining),
+    Hours: getHours(timeRemaining),
+    Minutes: getMinutes(timeRemaining),
+    Seconds: getSeconds(timeRemaining),
+  }
+  return humanReadableTime
+}
+
+export default UseCountdown
