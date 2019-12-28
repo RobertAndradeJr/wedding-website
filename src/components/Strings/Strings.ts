21944b78171ae39bb11164c42d63b675db58f35f
Robert Andrade <sandrade@wiley.com>
2019-12-28 03:54:54 -0600

    Strings file

---
+++ src/components/Strings/Strings.ts
@@ -0,0 +1,125 @@
+export const CountdownStrings = {
+  TITLE: 'Countdown: ',
+  WEDDING_DATE: 'Oct 31, 2020 18:00:00',
+}
+
+export const CityStrings = {
+  CITY_NAME: 'Minneapolis',
+  CITY_MOTTO: 'City of Lakes',
+}
+
+export const EngagementPicsStrings = {
+  TITLE: 'Engagement Pics',
+  SUBTITLE: 'Around the world and back again, with you',
+}
+
+export const EventsListStrings = {
+  TITLE: 'Events Poop',
+  DATE: 'October 31st',
+  CEREMONY: {
+    TITLE: 'Ceremony',
+    TIME: '5-6pm',
+    DESCRIPTION:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet nisl eget magna placerat ullamcorper eu eget turpis. Quisque accumsan maximus est nec efficitur. Proin ullamcorper tristique malesuada. In tempus, nisi a cursus ullamcorper, est nisl feugiat eros, id vulputate dolor odio ac sapien. Donec sit amet condimentum metus. In vitae viverra tortor, quis cursus ante. Phasellus venenatis orci eu luctus imperdiet. In hac habitasse platea dictumst. Nam at tempor nunc.',
+  },
+  COCKTAIL: {
+    TITLE: 'Cocktail Hour',
+    TIME: '6-7pm',
+    DESCRIPTION:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet nisl eget magna placerat ullamcorper eu eget turpis. Quisque accumsan maximus est nec efficitur. Proin ullamcorper tristique malesuada. In tempus, nisi a cursus ullamcorper, est nisl feugiat eros, id vulputate dolor odio ac sapien. Donec sit amet condimentum metus. In vitae viverra tortor, quis cursus ante. Phasellus venenatis orci eu luctus imperdiet. In hac habitasse platea dictumst. Nam at tempor nunc.',
+  },
+  DINNER: {
+    TITLE: 'Dinner from Chow Girls',
+    TIME: '7-8pm',
+    DESCRIPTION:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet nisl eget magna placerat ullamcorper eu eget turpis. Quisque accumsan maximus est nec efficitur. Proin ullamcorper tristique malesuada. In tempus, nisi a cursus ullamcorper, est nisl feugiat eros, id vulputate dolor odio ac sapien. Donec sit amet condimentum metus. In vitae viverra tortor, quis cursus ante. Phasellus venenatis orci eu luctus imperdiet. In hac habitasse platea dictumst. Nam at tempor nunc.',
+  },
+  DANCE: {
+    TITLE: 'Dancing the night away',
+    TIME: '8-midnight',
+    DESCRIPTION:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet nisl eget magna placerat ullamcorper eu eget turpis. Quisque accumsan maximus est nec efficitur. Proin ullamcorper tristique malesuada. In tempus, nisi a cursus ullamcorper, est nisl feugiat eros, id vulputate dolor odio ac sapien. Donec sit amet condimentum metus. In vitae viverra tortor, quis cursus ante. Phasellus venenatis orci eu luctus imperdiet. In hac habitasse platea dictumst. Nam at tempor nunc.',
+  },
+  DRESS_CODE: {
+    TITLE: 'Dress Code',
+    DESCRIPTION:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet nisl eget magna placerat ullamcorper eu eget turpis. Quisque accumsan maximus est nec efficitur. Proin ullamcorper tristique malesuada. In tempus, nisi a cursus ullamcorper, est nisl feugiat eros, id vulputate dolor odio ac sapien. Donec sit amet condimentum metus. In vitae viverra tortor, quis cursus ante. Phasellus venenatis orci eu luctus imperdiet. In hac habitasse platea dictumst. Nam at tempor nunc.',
+  },
+}
+
+export const FooterStrings = {
+  TITLE: '©2019 By ',
+  AUTHOR: 'Robert Andrade',
+  NOTE: '. Crafted with lots of love for Mandi',
+}
+
+export const HomeStrings = {
+  title: 'Robert & Mandi are getting married!',
+  detail: {
+    WHEN: 'When:',
+    WHERE: 'Where:',
+    ADDRESS: 'Address:',
+    WEDDING_DATE: 'October 31st, 2020',
+    WEDDING_LOCATION: 'Bell Museum',
+    WEDDING_ADDRESS: '2088 Larpenteur Ave W, St Paul, MN 55113',
+    WEDDING_CITY: 'St. Paul, MN',
+  },
+  buttonText: 'RSVP',
+  heroImage:
+    'https://instagram.ffcm1-2.fna.fbcdn.net/vp/16a383f1d9b3a06d6972085e6cf7ae1d/5DFDB388/t51.2885-15/e35/41203721_691517357888913_5430100155676178400_n.jpg?_nc_ht=instagram.ffcm1-2.fna.fbcdn.net',
+}
+
+export const HowWeMetStrings = {
+  TITLE: 'How we met',
+  DESCRIPTION:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet nisl eget magna placerat ullamcorper eu eget turpis. Quisque accumsan maximus est nec efficitur. Proin ullamcorper tristique malesuada. In tempus, nisi a cursus ullamcorper, est nisl feugiat eros, id vulputate dolor odio ac sapien. Donec sit amet condimentum metus. In vitae viverra tortor, quis cursus ante. Phasellus venenatis orci eu luctus imperdiet. In hac habitasse platea dictumst. Nam at tempor nunc.',
+}
+
+export const InstagramPicsStrings = {
+  PICTURE_WORTH_THOUSAND_WORDS: 'A picture is worth a thousand words!',
+  HELP_US: 'Help us capture the moment, tag your photos with the hashtag: ',
+  HASHTAG: 'wedding',
+}
+
+export const InvitationStrings = {
+  GETTING_HITCHED: 'We are getting hitched',
+}
+
+export const MapStrings = {
+  HOW_DO_I_GET_THERE: 'How do I get there?',
+  EASIER_THAN_YOU_THINK: 'It\'s way easier than you think!',
+  SHOW_INFO: 'Show info',
+  VENUE_NAME: 'Bell Museum',
+  VENUE_ADDRESS: '2088 Larpenteur Ave W,',
+  VENUE_ADDRESS_2: 'St Paul, MN 55113',
+  BOOK_UBER: 'Book an Uber',
+  SHOW_MAP: 'Show Map',
+  CONTACT: 'Contact Info',
+  CONTACT_NAME: 'Bellford Museumington',
+  CONTACT_NUMBER: '(612) 626-9660',
+}
+
+export const NavStrings = {
+  mobileTitle: 'Menu',
+  titleText: 'Robert & Mandi',
+  buttonLabels: [
+    'How We Met',
+    'Events',
+    'Engagement',
+    'Social',
+    'Glimpse of the city',
+    'Venue',
+    'RSVP',
+  ],
+  test: 'yeet',
+}
+
+export const RsvpStrings = {
+  RSVP: 'RSVP',
+}
+
+export const ShareBarStrings = {
+  TWITTER: 'Tweet #RobertMandiWedding',
+  FACEBOOK: 'Like us on Facebook',
+  INSTAGRAM: 'Post on Social',
+}
