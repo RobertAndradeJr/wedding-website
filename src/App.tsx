7566dfa293fc63a94d4a50e46a44d1113853e08f
Robert Andrade <sandrade@wiley.com>
2019-12-28 21:18:43 -0600

    Events list section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import './App.css';
 import Hero from './components/Hero';
+import NavBar from './components/NavBar';
+import EventsList from './components/EventsList';
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <NavBar />
       <Hero />
+      <EventsList />
     </div>
   );
 }
---
+++ src/components/EventsList.tsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal"
+import { EventsListStrings } from "./Strings/Strings";
+
+const {
+  TITLE,
+  DRESS_CODE,
+  CEREMONY,
+  COCKTAIL,
+  DINNER,
+  DANCE
+} = EventsListStrings;
+
+const columns = [
+  [CEREMONY, COCKTAIL],
+  [DINNER, DANCE]
+];
+
+const EventsList = () => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+  return (
+    <Container id="events" as="section" className="text-white text-center">
+      <Row>
+        <Col>
+          <h3 className="my-5">{TITLE}</h3>
+        </Col>
+      </Row>
+      <Row>
+        {columns.map((column, i) => (
+          <Col className={i === 0 ? "border-right border-white" : ""} key={i}>
+            {column.map(({ TITLE, TIME, DESCRIPTION }) => (
+              <Container key={TITLE}>
+                <Row>
+                  <Col>
+                    <h5>{TITLE}</h5>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <h6>{TIME}</h6>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <p>{DESCRIPTION}</p>
+                  </Col>
+                </Row>
+              </Container>
+            ))}
+          </Col>
+        ))}
+      </Row>
+      <Row className="my-5">
+        <Col>
+          <Button onClick={handleShow}>{DRESS_CODE.TITLE}</Button>
+        </Col>
+      </Row>
+      <Modal show={show} onHide={handleClose} size="lg" centered>
+        <Modal.Header>
+          <Modal.Title>
+            {DRESS_CODE.TITLE}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {DRESS_CODE.DESCRIPTION}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={handleClose}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    </Container>
+  );
+};
+
+export default EventsList;
