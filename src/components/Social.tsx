bd59cf89bd0b8444a3ccd502890e94ba98568bc2
Robert Andrade <sandrade@wiley.com>
2019-12-30 11:26:49 -0600

    New format for instagram pics

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -3,6 +3,7 @@ import { InstagramPicsStrings } from './Strings';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Figure from 'react-bootstrap/Figure';
 
 const { HASHTAG, HELP_US, PICTURE_WORTH_THOUSAND_WORDS } = InstagramPicsStrings;
 
@@ -44,11 +45,52 @@ const Social: React.FC = () => {
         </Col>
       </Row>
       <Row>
-        {Pictures.map((pic: Response) => (
-          <Col className="instagram--image" xs="6" md="3" key={pic.node.id}>
-            <img src={pic.node.display_url} alt="" />
-          </Col>
-        ))}
+        {Pictures.filter(pic => pic !== undefined).map(
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          (pic: Record<string, any>) => {
+            const link = `https://www.instagram.com/p/${pic.node.shortcode}/`;
+            const id = pic.node.id;
+            const thumb = pic.node.thumbnail_resources[2].src;
+            const caption =
+              pic?.node?.edge_media_to_caption?.edges[0]?.node?.text || '';
+            const captionLength = 140;
+            const trimCaption = (): string => {
+              if (!caption) {
+                return '';
+              } else if (caption.length >= captionLength) {
+                return `${caption.substring(0, captionLength)}...`;
+              } else {
+                return caption;
+              }
+            };
+            return (
+              <>
+                <Col className="instagram--image mw-100" xs="6" md="3" key={id}>
+                  <Figure>
+                    <a target="_blank" rel="noopener noreferrer" href={link}>
+                      <Figure.Image src={thumb} alt={caption || ''} />
+                    </a>
+                    <Figure.Caption>
+                      {trimCaption()} <br />{' '}
+                    </Figure.Caption>
+                  </Figure>
+                  {caption.length >= captionLength ? (
+                    <a
+                      className="mt-auto"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href={link}
+                    >
+                      View more on Instagram
+                    </a>
+                  ) : (
+                    ''
+                  )}
+                </Col>
+              </>
+            );
+          }
+        )}
       </Row>
     </Container>
   );
