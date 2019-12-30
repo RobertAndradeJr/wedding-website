9d2ded2f2473d747223fa5ca17b35dde108070bc
Robert Andrade <sandrade@wiley.com>
2019-12-30 11:40:19 -0600

    pic back to any type

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -45,52 +45,48 @@ const Social: React.FC = () => {
         </Col>
       </Row>
       <Row>
-        {Pictures.filter(pic => pic !== undefined).map(
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          (pic: Record<string, any>) => {
-            const link = `https://www.instagram.com/p/${pic.node.shortcode}/`;
-            const id = pic.node.id;
-            const thumb = pic.node.thumbnail_resources[2].src;
-            const caption =
-              pic?.node?.edge_media_to_caption?.edges[0]?.node?.text || '';
-            const captionLength = 140;
-            const trimCaption = (): string => {
-              if (!caption) {
-                return '';
-              } else if (caption.length >= captionLength) {
-                return `${caption.substring(0, captionLength)}...`;
-              } else {
-                return caption;
-              }
-            };
-            return (
-              <>
-                <Col className="instagram--image mw-100" xs="6" md="3" key={id}>
-                  <Figure>
-                    <a target="_blank" rel="noopener noreferrer" href={link}>
-                      <Figure.Image src={thumb} alt={caption || ''} />
-                    </a>
-                    <Figure.Caption>
-                      {trimCaption()} <br />{' '}
-                    </Figure.Caption>
-                  </Figure>
-                  {caption.length >= captionLength ? (
-                    <a
-                      className="mt-auto"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      href={link}
-                    >
-                      View more on Instagram
-                    </a>
-                  ) : (
-                    ''
-                  )}
-                </Col>
-              </>
-            );
-          }
-        )}
+        {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
+        {Pictures.filter(pic => pic !== undefined).map((pic: any) => {
+          const link = `https://www.instagram.com/p/${pic.node.shortcode}/`;
+          const id = pic.node.id;
+          const thumb = pic.node.thumbnail_resources[2].src;
+          const caption =
+            pic?.node?.edge_media_to_caption?.edges[0]?.node?.text || '';
+          const captionLength = 140;
+          const trimCaption = (): string => {
+            if (!caption) {
+              return '';
+            } else if (caption.length >= captionLength) {
+              return `${caption.substring(0, captionLength)}...`;
+            } else {
+              return caption;
+            }
+          };
+          return (
+            <>
+              <Col className="instagram--image mw-100" xs="6" md="3" key={id}>
+                <Figure>
+                  <a target="_blank" rel="noopener noreferrer" href={link}>
+                    <Figure.Image src={thumb} alt={caption || ''} />
+                  </a>
+                  <Figure.Caption>{trimCaption()}</Figure.Caption>
+                </Figure>
+                {caption.length >= captionLength ? (
+                  <a
+                    className="mt-auto"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href={link}
+                  >
+                    View more on Instagram
+                  </a>
+                ) : (
+                  ''
+                )}
+              </Col>
+            </>
+          );
+        })}
       </Row>
     </Container>
   );
