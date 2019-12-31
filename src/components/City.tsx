f887638b0e652e3b815d77493499cced1378e927
Robert Andrade <sandrade@wiley.com>
2019-12-30 21:43:28 -0600

    change video background to gif

--- src/components/City.tsx
+++ src/components/City.tsx
@@ -10,26 +10,22 @@ import YouTubeBackground from 'react-youtube-background';
 const { CITY_NAME, CITY_MOTTO } = CityStrings;
 
 const City: React.SFC = () => (
-  <Container as="section" className="text-center" id="glimpseofthecity" fluid>
-    <YouTubeBackground
-      videoId="CWzZ-Y2D9tw"
-      style={{ height: 420 }}
-      overlay="rgba(0,0,0,0.33)"
-      nocookie
-    >
-      <Jumbotron id="video-content">
-        <Row>
-          <Col>
-            <h5 className="display-4">{CITY_NAME}</h5>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <p className="display-5">{CITY_MOTTO}</p>
-          </Col>
-        </Row>
-      </Jumbotron>
-    </YouTubeBackground>
+  <Container
+    as="section"
+    className="text-center parallax city"
+    id="glimpseofthecity"
+    fluid
+  >
+    <Jumbotron className="video-content">
+      <Row>
+        <Col xs="12">
+          <h5 className="display-4">{CITY_NAME}</h5>
+        </Col>
+        <Col xs="12">
+          <p className="display-5">{CITY_MOTTO}</p>
+        </Col>
+      </Row>
+    </Jumbotron>
   </Container>
 );
 
