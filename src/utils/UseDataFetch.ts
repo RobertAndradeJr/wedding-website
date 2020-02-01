9e780bd5fd967a916aa25547be8b8cd01040bb65
Robert Andrade <sandrade@wiley.com>
2020-02-01 11:35:02 -0600

    clean up fetch

--- src/utils/UseDataFetch.ts
+++ src/utils/UseDataFetch.ts
@@ -10,12 +10,14 @@ const UseFetch = (url: string): InstagramResponse => {
   const [error, setError] = useState({});
   useEffect(() => {
     const abortController = new AbortController();
-    const { signal } = abortController;
-    const opts = { signal };
-    fetch(url, opts)
-      .then(res => res.json())
-      .then(json => setData(json.graphql.hashtag.edge_hashtag_to_media.edges))
-      .catch(error => setError(error));
+    const fetchData = async (): Promise<boolean | void> =>
+      await fetch(url, { signal: abortController.signal })
+        .then(res => res.json())
+        .then(({ graphql }) =>
+          setData(graphql.hashtag.edge_hashtag_to_media.edges)
+        )
+        .catch(error => !abortController.signal.aborted && setError(error));
+    fetchData();
     return (): void => {
       abortController.abort();
     };
