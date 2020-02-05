8d284fe935b79d1544989b4afe84d118465b9e4e
Robert Andrade <sandrade@wiley.com>
2020-02-04 19:42:26 -0600

    lint

--- src/components/animations/SheepAnimation.tsx
+++ src/components/animations/SheepAnimation.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
 
-export const SheepAnimation: React.FC = () => {
-  return (
-    <div className="sheep">
-      <div className="top">
-        <div className="body" />
-        <div className="head">
-          <div className="eye one" />
-          <div className="eye two" />
-          <div className="ear one" />
-          <div className="ear two" />
-        </div>
-      </div>
-      <div className="legs">
-        <div className="leg" />
-        <div className="leg" />
-        <div className="leg" />
-        <div className="leg" />
+export const SheepAnimation: React.FC = () => (
+  <div className="sheep">
+    <div className="top">
+      <div className="body" />
+      <div className="head">
+        <div className="eye one" />
+        <div className="eye two" />
+        <div className="ear one" />
+        <div className="ear two" />
       </div>
     </div>
-  );
-};
+    <div className="legs">
+      <div className="leg" />
+      <div className="leg" />
+      <div className="leg" />
+      <div className="leg" />
+    </div>
+  </div>
+);
 
 export default SheepAnimation;
